44b4d48a2f7e3ddf4f250aa8a4e1f74bb6022645
Romain TOUZE <romain@rtodev.tech>
2024-06-10 15:51:39 +0200

    ajoute de features avec TDD

--- src/metier/panier.ts
+++ src/metier/panier.ts
@@ -7,10 +7,13 @@ export interface PanierRepository {
 
 export class Panier {
 
+  private items: Array<Item>
+
   constructor(
     public readonly id:string,
     private references: Array<string>
   ) {
+    this.items = []
   }
 
   ajouterReference(reference: string) {
@@ -25,13 +28,49 @@ export class Panier {
   toDTO(): PanierDTO {
     return {
       id: this.id,
-      references: this.references
+      references: this.references.map(r => r)
     }
   }
 
   getReferences(): Array<string> {
     return this.references.map(r => r)
   }
+
+  ajouterItems(reference: string, quantite: number): void {
+    const index= this.items.findIndex(i => i.reference === reference)
+    if (index > -1) {
+      this.items[index].quantite += quantite
+      return
+    } 
+
+    this.items.push({reference: reference, quantite:quantite})
+  }
+
+  incrementerItem(reference: string) {
+    this.ajouterItems(reference, 1)
+  }
+
+  decrementerItem(reference: string) {
+    const index= this.items.findIndex(i => i.reference === reference)
+    this.items[index].quantite -= 1
+    if (this.items[index].quantite == 0) {
+      this.retirerItem(reference)
+    }
+  }
+
+  retirerItem(reference:string) {
+    const index= this.items.findIndex(i => i.reference === reference)
+    this.items.splice(index,1)
+  }
+
+  getItems():Array<Item> {
+    return this.items
+  }
+}
+
+type Item = {
+  reference: string,
+  quantite: number
 }
 
 export interface PanierPresenter {
--- src/tests/panier.spec.ts
+++ src/tests/panier.spec.ts
@@ -1,12 +1,126 @@
-import {describe, expect, test} from '@jest/globals'
-import {Panier} from '../metier/panier'
+import { describe, expect, test } from "@jest/globals"
+import { Panier } from "../metier/panier"
 
 describe("Le panier", () => {
-  test("doit permttre d'ajouter une reference", () => {
+  test("doit permettre d'ajouter une reference", () => {
     const p = new Panier("foobar", [])
     p.ajouterReference("ref1")
     expect(p.getReferences()).toHaveLength(1)
     expect(p.getReferences()[0]).toEqual("ref1")
   })
+
+  test("doit permettre de retirer une reference", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterReference("ref1")
+    p.ajouterReference("ref2")
+    p.retirerReference("ref1")
+    expect(p.getReferences()).toHaveLength(1)
+    expect(p.getReferences()[0]).toEqual("ref2")
+  })
+
+  test("doit permettre de restituer son contenu sous forme de DTO", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterReference("ref1")
+    p.ajouterReference("ref2")
+    const dto = p.toDTO()
+    expect(dto).toEqual({
+      id: "foobar",
+      references: ["ref1", "ref2"],
+    })
+  })
+
+  test("doit être initialisé avec une liste de références", () => {
+    const p = new Panier("foobar", ["ref1", "ref2"])
+    expect(p.toDTO()).toEqual({
+      id: "foobar",
+      references: ["ref1", "ref2"],
+    })
+  })
+
+  test("ne doit pas être impacté par des modifications du DTO", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterReference("ref1")
+    p.ajouterReference("ref2")
+    const dto = p.toDTO()
+    dto.references.pop()
+    expect(p.getReferences()).toEqual(["ref1", "ref2"])
+  })
+
+  test("doit permettre d'ajouter un item de quantite 1", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 1)
+    expect(p.getItems()).toEqual([
+      {
+        reference: "ref1",
+        quantite: 1,
+      },
+    ])
+  })
+
+  test("doit permettre d'ajouter un item de quantite 5", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    expect(p.getItems()).toEqual([
+      {
+        reference: "ref1",
+        quantite: 5,
+      },
+    ])
+  })
+
+  test("doit permettre d'incrémenter une quantite d'item", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    p.incrementerItem("ref1")
+    expect(p.getItems()).toEqual([
+      {
+        reference: "ref1",
+        quantite: 6,
+      },
+    ])
+  })
+
+  test("doit permettre de décrémenter une quantité d'item", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    p.decrementerItem("ref1")
+    expect(p.getItems()).toEqual([
+      {
+        reference: "ref1",
+        quantite: 4,
+      },
+    ])
+  })
+
+  test("doit permettre d'ajouter des items en gardant une ligne par reference", () => {
+
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    p.ajouterItems("ref1", 4)
+    expect(p.getItems()).toEqual([
+      {
+        reference: "ref1",
+        quantite: 9,
+      },
+    ])
+  })
+
+  test("doit retirer l'item de la liste si sa quantite est nulle", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    for (let i = 0; i <5; i++) {
+      p.decrementerItem("ref1")
+    }
+
+    expect(p.getItems()).toEqual([])
+  })
+
+  test("doit retirer un item", () => {
+    const p = new Panier("foobar", [])
+    p.ajouterItems("ref1", 5)
+    p.retirerItem("ref1")
+    expect(p.getItems()).toEqual([])
+  })
 })
 
+// vim: fdm=indent
