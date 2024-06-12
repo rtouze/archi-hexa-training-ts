ac68da6fa71ccd3a0973ac91560ff9fb6b783da7
Romain TOUZE <romain@rtodev.tech>
2024-06-12 12:27:39 +0200

    implementation PanierRepositoryDb

--- src/infra/panier.ts
+++ src/infra/panier.ts
@@ -1,8 +1,6 @@
-import {
-  PanierRepository,
-  Panier,
-} from "../metier/panier"
-import { Client as PgClient } from 'pg'
+import { PanierRepository, Panier } from "../metier/panier"
+import { Article, Quantite, ProduitBuilder } from "../metier/values"
+import { Client as PgClient } from "pg"
 
 type Collection<T> = {
   [id: string]: T
@@ -36,21 +34,57 @@ export class PanierRepositoryEnMemoire implements PanierRepository {
   }
 }
 
+type ArticleData = {
+  quantity: number
+  sku: string
+  gtin: string
+}
+
 export class PanierRepositoryDb implements PanierRepository {
-  constructor(private readonly client: PgClient) {
-    
-  }
+  constructor(private readonly client: PgClient) {}
   async sauver(panier: Panier): Promise<void> {
     const dto = panier.toDto()
-    await this.client.query("INSERT INTO panier (uuid) VALUES ($1) ON CONFLICT (uuid) DO NOTHING", [dto.id])
+
+    await this.client.query(
+      "INSERT INTO panier (uuid) VALUES ($1) ON CONFLICT (uuid) DO NOTHING",
+      [dto.id],
+    )
+
+    await this.client.query("DELETE FROM article WHERE panier_id = $1", [
+      dto.id,
+    ])
+
+    dto.articles.forEach(async (a) => {
+      await this.client.query(
+        "INSERT INTO article (panier_id, quantity, sku) VALUES ($1, $2, $3)",
+        [dto.id, a.quantite, a.produit.sku],
+      )
+    })
   }
 
   async recuperer(panierId: string): Promise<Panier> {
-    const data = await this.client.query("SELECT 1 FROM panier WHERE uuid = $1", [panierId])
+    const data = await this.client.query(
+      "SELECT 1 FROM panier WHERE uuid = $1",
+      [panierId],
+    )
     if (data.rows.length === 0) {
-      // TODO
-      throw new Error("")
+      throw new Error(`Panier ${panierId} non trouvé`)
     }
-    return new Panier(panierId, [])
+    const articleData = await this.client.query<ArticleData>(
+      `
+    select a.quantity, c.sku, c.gtin
+    from article a join catalogue c on c.sku = a.sku
+    where a.panier_id = $1`,
+      [panierId],
+    )
+
+    const articles = articleData.rows.map((ad) => {
+      return new Article(
+        new ProduitBuilder().avecSku(ad.sku).avecGtin(ad.gtin).creer(),
+        new Quantite(ad.quantity),
+      )
+    })
+
+    return new Panier(panierId, articles)
   }
 }
--- src/tests_integration/panierRepository.test.ts
+++ src/tests_integration/panierRepository.test.ts
@@ -3,10 +3,13 @@ import { Client } from 'pg'
 import {PanierRepositoryDb} from '../infra/panier'
 import { v4 as uuidv4 } from 'uuid'
 import { Panier } from '../metier/panier'
+import { ProduitBuilder, Quantite } from '../metier/values'
 
 
 describe("PanierRepositoryDb", () => {
   let client:Client
+  let repo:PanierRepositoryDb
+
   beforeAll(async () => {
     client = new Client({
       connectionString: "postgres://enslipch:secret@localhost:8888/enslipch",
@@ -15,17 +18,41 @@ describe("PanierRepositoryDb", () => {
   })
   beforeEach(async () => {
     await client.query('begin')
+    repo = new PanierRepositoryDb(client)
   })
   afterEach(async () => {
     await client.query('rollback')
   })
   test("peut sauver un panier vide", async () => {
-    const repo = new PanierRepositoryDb(client)
     const panierId = uuidv4()
     await repo.sauver(new Panier(panierId, []))
     const nouveauPanier = await repo.recuperer(panierId)
-    const resp = await client.query<{uuid: string}>("select uuid from panier")
     expect(nouveauPanier.toDto().id).toEqual(panierId)
+  })
 
+  test("renvoie une exception si le panier n'est pas trouvé", async () => {
+    const panierId = uuidv4()
+    try {
+      await repo.recuperer(panierId)
+    } catch (e) {
+      expect(e).toEqual(new Error(`Panier ${panierId} non trouvé`))
+    }
+  })
+
+  test("peut sauver un panier avec articles", async () => {
+    const panierId = uuidv4()
+    const panier = new Panier(panierId, [])
+    const p = new ProduitBuilder().avecSku('slip-noir').avecGtin('12345').creer()
+    const p2 = new ProduitBuilder().avecSku('slip-blanc').avecGtin('12346').creer()
+    panier.ajouterArticle(p, new Quantite(3))
+    panier.ajouterArticle(p2, new Quantite(6))
+    await repo.sauver(panier)
+    const nouveauPanier = await repo.recuperer(panierId)
+    const dto = nouveauPanier.toDto()
+    expect(dto.articles).toHaveLength(2)
+    expect(dto.articles[0].quantite).toEqual(3)
+    expect(dto.articles[1].quantite).toEqual(6)
+    expect(dto.articles[0].produit).toEqual({sku:'slip-noir', gtin: '12345'})
+    expect(dto.articles[1].produit).toEqual({sku:'slip-blanc', gtin:  '12346'})
   })
 })
